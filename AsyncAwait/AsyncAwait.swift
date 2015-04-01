39d85d1a77f8e3000f28e2939d4f62459e5d7cc1
Adam <adam@nullriver.com>
2015-03-31 21:14:29 -0400

    Cleaned up Await with timeout

--- AsyncAwait/AsyncAwait.swift
+++ AsyncAwait/AsyncAwait.swift
@@ -53,22 +53,26 @@ public func Async<T>(priority: Priority = .Default, call: Void -> T) -> Task<T> 
 
 /**
 Wait or retrieve a result of a task.
-(If timeout happens, the task can Await again.)
 
 :param: task Task object created by Async.
-:param: timeout Milliseconds after the task is abandoned. (Default is never abandoned)
 :returns: Result of the task or nil if it timed out.
 */
 public func Await<T>(task: Task<T>) -> T {
-    //let nanotimeout: UInt64 = (timeout == 0) ? DISPATCH_TIME_FOREVER : UInt64(timeout) * 1000000
     dispatch_group_wait(task.group, DISPATCH_TIME_FOREVER)
     let result = task.result
     task.result = nil // clear the result so it can only get the result once, twice will always return nil
     return result!
 }
 
+/**
+Wait or retrieve a result of a task with a timeout.
+(If timeout happens, the task can Await again.)
 
-public func Await<T>(task: Task<T>, timeout: Int) -> T? {
+:param: task Task object created by Async.
+:param: timeout Milliseconds after the task is abandoned. (Default is never abandoned)
+:returns: Result of the task or nil if it timed out.
+*/
+public func Await<T>(task: Task<T>, #timeout: Int) -> T? {
     let nanotimeout: UInt64 = UInt64(timeout) * 1000000
     dispatch_group_wait(task.group, nanotimeout)
     let result = task.result
