4db0be5a8eb29d31f10aafafc4055e452d7f50cf
Adam <adam@nullriver.com>
2015-06-18 21:52:22 -0400

    Changed to a Framework

---
+++ AsyncAwait/AsyncAwait.h
@@ -0,0 +1,19 @@
+//
+//  AsyncAwait.h
+//  AsyncAwait
+//
+//  Created by Adam Dann on 2015-06-18.
+//  Copyright © 2015 Adam Dann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AsyncAwait.
+FOUNDATION_EXPORT double AsyncAwaitVersionNumber;
+
+//! Project version string for AsyncAwait.
+FOUNDATION_EXPORT const unsigned char AsyncAwaitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncAwait/PublicHeader.h>
+
+
